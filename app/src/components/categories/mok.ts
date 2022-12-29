74026bf58174429fae88c92f7987d713643aaf1b
Rafael Pereira <rafaengcivilrj@outlook.com>
2022-12-29 19:48:08 -0300

    lista de produtos iniciada

--- app/src/components/categories/mok.ts
+++ app/src/components/categories/mok.ts
@@ -2,22 +2,32 @@
   export const CategoriesList = [
     {
       _id: '651351646511asdasda',
-      name:'Pizzas'
+      name:'Pizzas',
+      descritption:'lorekasdbkjabsddwas',
+      price: ' 42'
     },
     {
       _id: '651351646511asdasd4',
-      name:'Bebidas'
+      name:'Bebidas',
+      descritption:'lorekasdbkjabsddwas',
+      price: ' 21'
     },
     {
       _id: '651351646511asdasd5',
-      name:'Burgers'
+      name:'Burgers',
+      descritption:'lorekasdbkjabsddwas',
+      price: ' 50'
     },
     {
       _id: '6351646511asdasd5',
-      name:'Salgados'
+      name:'Salgados',
+      descritption:'lorekasdbkjabsddwas',
+      price: ' 1'
     },
     {
       _id: '651351646511asdasd6',
-      name:'Promoções'
+      name:'Promoções',
+      descritption:'lorekasdbkjabsddwas',
+      price: ' 5'
     },
   ];
--- app/src/components/menu/index.tsx
+++ app/src/components/menu/index.tsx
@@ -1,3 +1,27 @@
+import { FlatList } from "react-native";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { CategoriesList as products } from "../categories/mok";
+import { Text } from "../Text";
+import {Product, ProductImage, ProductDetails} from "./styles";
+
 export function Menu (){
-return null
+return (
+  <FlatList
+    data={products}
+    style={{marginTop: 32 }}
+    contentContainerStyle={{paddingHorizontal: 24 }}
+    keyExtractor={product => product._id}
+    renderItem={({ item: product}) => (
+      <Product>
+        <ProductImage
+          source={{uri:`http://localhost:3000/uploados/${product.imagePath}`}}
+        />
+        <ProductDetails>
+          <Text weight="600">{product.name}</Text>
+          <Text size={14} color='#666' style={{ marginVertical: 8 }}>{product.descritption}</Text>
+          <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
+        </ProductDetails>
+      </Product>
+    )}/>
+)
 }
